605ff4d7fd832f37c092efba1dad937ce87052ae
Cosmas Wasswa <cosywas@gmail.com>
2023-09-20 01:18:37 +0300

    Fix dependency bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
